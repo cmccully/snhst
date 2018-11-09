17fcda397453abcc32b2ff4d17b160f5f5e3f9be
Curtis McCully <cmccully@lco.global>
2018-11-09 18:37:36 +0000

    Bumped version to 0.10.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='snhst',
       author=['Curtis McCully'],
       author_email=['cmccully@lco.global'],
-      version=0.9,
+      version=0.10.0,
       packages=['snhst'],
       setup_requires=[],
       install_requires=['numpy', 'astropy', 'matplotlib', 'drizzlepac', 'astroscrappy', 'reproject',
